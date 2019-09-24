c1b20ee763c869313a34ef7f162423a04a6cb3d1
Lorry <Lorry1123@outlook.com>
2019-09-24 21:49:37 +0800

    ADD: react root to lottery page

--- core/static/js/lottery.jsx
+++ core/static/js/lottery.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+const { useState } = React;
 
 const LotteryPage = (props) => {
     return (
--- core/view/web.py
+++ core/view/web.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-web_bp = Blueprint('web', __name__, template_folder="../template")
+web_bp = Blueprint('web', __name__, url_prefix="/lottery", template_folder="../template", static_folder="../static")
 
 @web_bp.route("/")
 def render():
